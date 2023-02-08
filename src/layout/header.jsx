b5a503c8067916d7474ec1b1795dadfe7447a439
ibrohim-yusupjonov <110836470+ibrohim-yusupjonov@users.noreply.github.com>
2023-02-08 10:07:51 +0500

    adds

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,8 +1,8 @@
-import logo from "./logo.svg"
+import logo from "./logo.png"
 function Header() {
     return (
         <div className="header">
-            <img src={logo} width="70px"></img>
+            <img src={logo} width="100px"></img>
             <div className='logo'>logo</div>
             <div className="header-nav">
                 <ul>
