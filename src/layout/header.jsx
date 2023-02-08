b09ec62e3f9cee7067d04ab17e4802327f5f2494
Jahongir <akhrasmiy@gmail.com>
2023-02-08 10:26:37 +0500

    akh

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,9 +1,8 @@
-import logo from "./Untitled1.png"
+import logo from "./logo.png"
 function Header() {
     return (
         <div className="header">
             <img src={logo} width="100px" height="100px"></img>
-            <div className='logo'>logo</div>
             <div className="header-nav">
                 <ul>
                     <li>Topib oldim</li>
